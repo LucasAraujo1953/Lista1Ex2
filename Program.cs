f68badec0f06377148cbb36459ea96a56e73b7ee
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-12 20:15:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Informe a aresta do quadrado: ");
+            a=int.Parse(Console.ReadLine());
+
+            b = a * a;
+            Console.WriteLine("O valor da aresta do quadrado é {0}, com isso, sua área é igual a {1}", a, b);
+        }
+    }
+}
